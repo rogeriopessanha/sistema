f16814d918a15ec1d0a12ce2e7363f59959e22ca
Rogerio Pessanha <rogerioo_pessanha@yahoo.com.br>
2023-01-11 01:00:28 -0300

    att

--- sistema/src/pages/Dashboard/index.js
+++ sistema/src/pages/Dashboard/index.js
@@ -3,11 +3,14 @@ import { useContext } from "react"
 
 import { AuthContext } from "../../contexts/auth"
 
+import Header from '../../components/Header'
+
 export default function Dashboard() {
 
     const {signOut} = useContext(AuthContext)
     return(
         <div>
+            <Header/>
             <h1>Página Dashboard</h1>
             <button onClick={ () => signOut() } >Sair</button>
         </div>
