be97a1894dfd08ff0b048b319dabe632d1affafa
Rogerio Pessanha <rogerioo_pessanha@yahoo.com.br>
2023-01-14 17:12:21 -0300

    att

--- sistema/src/pages/Dashboard/index.js
+++ sistema/src/pages/Dashboard/index.js
@@ -6,9 +6,9 @@ import { useState, useEffect } from 'react'
 
 import Header from '../../components/Header'
 import Title from '../../components/Title'
-import { FiMessageSquare, FiPlus, FiSearch,FiEdit2 } from 'react-icons/fi'
+import { FiMessageSquare, FiPlus, FiSearch, FiEdit2 } from 'react-icons/fi'
 import { Link } from 'react-router-dom'
-import{format} from 'date-fns'
+import { format } from 'date-fns'
 import firebase from '../../services/firebaseConnection'
 
 const listRef = firebase.firestore().collection('chamados').orderBy('created', 'desc')
@@ -20,36 +20,37 @@ export default function Dashboard() {
     const [isEmpty, setIsEmpty] = useState(false)
     const [lastDocs, setLastDocs] = useState()
 
-    useEffect(() =>{
+    useEffect(() => {
 
-        loadingChamados()
-        
-        return() => {
+        loadChamados()
+
+        return () => {
 
         }
     }, [])
 
-    async function loadingChamados() {
+    async function loadChamados() {
         await listRef.limit(5)
-        .get()
-        .then((snapshort) =>{
-            updateState(snapshort)
-        })
-        .catch((err) =>{
-            console.log('Deu algum erro', err)
-            setLoadingMore(false)
-        })
+            .get()
+            .then((snapshort) => {
+                updateState(snapshort)
+            })
+            .catch((err) => {
+                console.log('Deu algum erro', err)
+                setLoadingMore(false)
+            })
 
         setLoading(false)
     }
 
-    async function updateState(snapshort) {
-        const isCollectionEmpty = snapshort.size === 0
+
+    async function updateState(snapshot) {
+        const isCollectionEmpty = snapshot.size === 0;
 
         if (!isCollectionEmpty) {
-            let lista = []
+            let lista = [];
 
-            snapshort.forEach((doc) => {
+            snapshot.forEach((doc) => {
                 lista.push({
                     id: doc.id,
                     assunto: doc.data().assunto,
@@ -60,20 +61,45 @@ export default function Dashboard() {
                     status: doc.data().status,
                     complemento: doc.data().complemento
                 })
-            });
+            })
 
-            const lastDoc = snapshort.docs[snapshort.docs.length -1]
+            const lastDoc = snapshot.docs[snapshot.docs.length - 1]; //Pegando o ultimo documento buscado
 
-            setChamados(chamados => [...chamados, ...lista])
-            setLastDocs(lastDoc)
-        }else{
-            setIsEmpty(true)
+            setChamados(chamados => [...chamados, ...lista]);
+            setLastDocs(lastDoc);
+
+        } else {
+            setIsEmpty(true);
         }
 
-        setLoadingMore(false)
+        setLoadingMore(false);
+
     }
 
-   
+    function handleMore() {
+        
+    }
+
+    if (loading) {
+        return (
+            <div>
+                <Header />
+
+                <div className="content">
+                    <Title name='Atendimentos'>
+                        <FiMessageSquare size={20} />
+                    </Title>
+
+                    <div className="container-dashboard">
+                        <span>Buscando chamado...</span>
+                    </div>
+                </div>
+            </div>
+        )
+    }
+
+
+
     return (
         <div>
             <Header />
@@ -92,9 +118,9 @@ export default function Dashboard() {
                             Novo chamado
                         </Link>
                     </div>
-                )  :(
-                   <>
-                   <Link to='/novo' className="novo">
+                ) : (
+                    <>
+                        <Link to='/novo' className="novo">
                             <FiPlus size={20} color='#fff' />
                             Novo chamado
                         </Link>
@@ -111,26 +137,33 @@ export default function Dashboard() {
                             </thead>
 
                             <tbody>
-                                <tr>
-                                    <td data-label='Cliente'>Sujeito</td>
-                                    <td data-label='Assunto'>Assunto</td>
-                                    <td data-label='Status'>
-                                        <span className="andamento" style={{backgroundColor: '#5cb85c'}}>Em aberto</span>
-                                    </td>
-                                    <td data-label='Cadastrado'>12/01/2023</td>
-                                    <td data-label='#'>
-                                        <button className="btn-acao" style={{backgroundColor: '#3583f6'}}>
-                                            <FiSearch color='#fff' size={16}/>
-                                        </button>
+                                {chamados.map((item, index) => {
+                                    return (
+                                        <tr key={index}>
+                                            <td data-label='Cliente'>{item.cliente}</td>
+                                            <td data-label='Assunto'>{item.assunto}</td>
+                                            <td data-label='Status'>
+                                                <span className="andamento" style={{ backgroundColor: item.status === 'Aberto' ?  '#5cb85c' : '#999' }}>{item.status}</span>
+                                            </td>
+                                            <td data-label='Cadastrado'>{item.createdFormated}</td>
+                                            <td data-label='#'>
+                                                <button className="btn-acao" style={{ backgroundColor: '#3583f6' }}>
+                                                    <FiSearch color='#fff' size={16} />
+                                                </button>
 
-                                        <button className="btn-acao" style={{backgroundColor: '#f6a935'}}>
-                                            <FiEdit2 color='#fff' size={16}/>
-                                        </button>
-                                    </td>
-                                </tr>
+                                                <button className="btn-acao" style={{ backgroundColor: '#f6a935' }}>
+                                                    <FiEdit2 color='#fff' size={16} />
+                                                </button>
+                                            </td>
+                                        </tr>
+                                    )
+                                })}
                             </tbody>
                         </table>
-                   </> 
+
+                        {loadingMore && <h3 style={{textAlign: 'center', marginTop: 15}}>Buscando dados...</h3>}
+                        { !loadingMore && !isEmpty && <button className="btn-more" onClick={handleMore}>Buscar mais</button>}
+                    </>
                 )}
 
 
